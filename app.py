49ef279015bd30faa437199a2b35927214571737
Stijn Servaes <stijnservaes19@gmail.com>
2023-09-26 18:15:07 -0600

    Created simple file uploader and updated docker

---
+++ app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import streamlit as st
+
+uploaded_file = st.file_uploader("Upload a DOC file", type=["csv", "xlsx", "xls"])
+
+if uploaded_file is not None:
+    file_extension = uploaded_file.name.split(".")[-1].lower()
+    st.write("File Name:", uploaded_file.name)
+    st.write("File Content:")
+    if file_extension == "csv":
+        data = pd.read_csv(uploaded_file)
+    else:
+        data = pd.read_excel(uploaded_file)
+    st.write(data)
