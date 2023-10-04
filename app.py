6b40f2d1d89b0c996caa0b4f5851087f5be2edb8
Stijn Servaes <stijnservaes19@gmail.com>
2023-10-04 09:41:17 -0600

    updated requirements and inserted first prompt

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import streamlit as st
+import os
+from langchain.agents import create_pandas_dataframe_agent 
+from langchain.llms import OpenAI
 
 uploaded_file = st.file_uploader("Upload an excel file", type=["csv", "xlsx", "xls"])
 
 api_key = st.text_input("Enter your OpenAI API Key")
 api_key = st.secrets.openai_credentials.api_key
+
+os.environ['OPENAI_API_KEY'] = api_key
 
 if uploaded_file is not None:
     file_extension = uploaded_file.name.split(".")[-1].lower()
@@ -14,4 +19,11 @@ if uploaded_file is not None:
         data = pd.read_csv(uploaded_file)
     else:
         data = pd.read_excel(uploaded_file)
-    st.write(data)
+    st.write('File loaded successfully.')
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0), data, verbose=True) 
+    openai = OpenAI(temperature=0.0) 
+    st.write(openai.model_name)
+    prompt = st.text('What would you like to do?')
+
+    if prompt is not None and api_key is not None:
+        agent(prompt)
